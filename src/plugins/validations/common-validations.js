882561b311110f87294294904f58f21e2010516e
TikeSanOo-softguide <softguide.tikesanoo@gmail.com>
2025-07-03 09:11:02 +0630

    add validation

---
+++ src/plugins/validations/common-validations.js
@@ -0,0 +1,24 @@
+// Import yup
+import * as yup from 'yup';
+
+// Export reusable schemas
+export const loginSchema = yup.object({
+  email: yup
+    .string()
+    .required('Email is required')
+    .email('Invalid email format'),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(6, 'At least 6 characters'),
+});
+
+export const registerSchema = yup.object({
+  username: yup.string().required('Username is required'),
+  email: yup.string().required().email(),
+  password: yup.string().required().min(6),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password')], 'Passwords must match')
+    .required('Confirm your password'),
+});
