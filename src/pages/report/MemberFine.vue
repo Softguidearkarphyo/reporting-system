4c2d0bbe3f650fea8d74d9bce4c2b358d4b1519b
TikeSanOo-softguide <softguide.tikesanoo@gmail.com>
2025-08-06 10:16:53 +0630

    update src

--- src/pages/report/MemberFine.vue
+++ src/pages/report/MemberFine.vue
@@ -1,40 +1,40 @@
 <template>
   <BaseTitle> {{ t('memberFine.title1') }} </BaseTitle>
   <ParentCard class="pa-2">
-     <v-row class="align-center">
-       <v-col cols="12" md="12">
-         <Form
-         ref="formRef"
-         :validation-schema="memberFineValidation"
-         @submit="submit"
-         >
-            <div class="d-flex flex-wrap align-center justify-space-around">
-                <Field name="staff" v-slot="{ field, errorMessage }" >
-                  <BaseSelect
-                    v-model="field.value"
-                    v-bind="field"
-                    item-value="id"
-                    item-title="name"
-                    :items="items"
-                    :label="t('memberFine.form.name')"
-                    variant="plain"
-                    prependIcon="mdi-account-tie"
-                    :error-messages="errorMessage"
-                    :width="'300px'"
-                  />
-                </Field>
-                <Field name="date" v-slot="{ field, errorMessage }" >
-                  <BaseDatePicker
-                    v-model="field.value"
-                    v-bind="field"
-                    :label="t('memberFine.form.date')"
-                    prependIcon="mdi-calendar-month"
-                    :error-messages="errorMessage"
-                    :width="'300px'"
-                    style="flex: none"
-                  />
-                </Field>
-                <!-- <Field name="time"  v-slot="{ field, errorMessage }">
+    <v-row class="align-center">
+      <v-col cols="12" md="12">
+        <Form
+          ref="formRef"
+          :validation-schema="memberFineValidation"
+          @submit="submit"
+        >
+          <div class="d-flex flex-wrap align-center justify-space-around">
+            <Field name="staff" v-slot="{ field, errorMessage }">
+              <BaseSelect
+                v-model="field.value"
+                v-bind="field"
+                item-value="id"
+                item-title="name"
+                :items="items"
+                :label="t('memberFine.form.name')"
+                variant="plain"
+                prependIcon="mdi-account-tie"
+                :error-messages="errorMessage"
+                :width="'300px'"
+              />
+            </Field>
+            <Field name="date" v-slot="{ field, errorMessage }">
+              <BaseDatePicker
+                v-model="field.value"
+                v-bind="field"
+                :label="t('memberFine.form.date')"
+                prependIcon="mdi-calendar-month"
+                :error-messages="errorMessage"
+                :width="'300px'"
+                style="flex: none"
+              />
+            </Field>
+            <!-- <Field name="time"  v-slot="{ field, errorMessage }">
                   <BaseTimePicker  
                     v-model="field.value"
                     v-bind="field"
@@ -43,26 +43,26 @@
                     prependIcon="mdi-clock-outline"
                     :error-messages="errorMessage" />
                 </Field> -->
-              
-                <Field name="time" v-slot="{ field, errorMessage }" >
-                  <BaseSelect
-                    v-model="field.value"
-                      v-bind="field"
-                      :label="t('memberFine.form.time')"
-                      :items="lateTimes"
-                      prependIcon="tabler:IconClockHour5"
-                      item-title="name"
-                      item-value="id"
-                      :error-messages="errorMessage"
-                      :width="'300px'"
-                    >
-                  </BaseSelect>
-                </Field>
-                <BaseButton type="submit"  :width="'200px'">
-                  {{ t('common.submit') }}
-                </BaseButton>
-              </div>
-          </Form>
+
+            <Field name="time" v-slot="{ field, errorMessage }">
+              <BaseSelect
+                v-model="field.value"
+                v-bind="field"
+                :label="t('memberFine.form.time')"
+                :items="lateTimes"
+                prependIcon="tabler:IconClockHour5"
+                item-title="name"
+                item-value="id"
+                :error-messages="errorMessage"
+                :width="'300px'"
+              >
+              </BaseSelect>
+            </Field>
+            <BaseButton type="submit" :width="'200px'">
+              {{ t('common.submit') }}
+            </BaseButton>
+          </div>
+        </Form>
       </v-col>
     </v-row>
   </ParentCard>
@@ -70,77 +70,84 @@
   <div v-if="status">
     <ParentCard>
       <div class="d-flex justify-space-around align-center">
-          <BaseSelect
-              v-model="selectedName"
-              :label="t('memberFine.form.name')"
-              item-value="name"
-              item-title="name"
-              :items="items"
-              :width="'500px'"
-              >
-          </BaseSelect>
-          <BaseSelect
-              v-model="selectedMonth"
-              item-value="id"
-              item-title="name"
-              :items = "months"
-              :label="t('memberFine.form.month')"
-              :width="'500px'"
-            >
-          </BaseSelect>
+        <BaseSelect
+          v-model="selectedName"
+          :label="t('memberFine.form.name')"
+          item-value="name"
+          item-title="name"
+          :items="items"
+          :width="'500px'"
+        >
+        </BaseSelect>
+        <BaseSelect
+          v-model="selectedMonth"
+          item-value="id"
+          item-title="name"
+          :items="months"
+          :label="t('memberFine.form.month')"
+          :width="'500px'"
+        >
+        </BaseSelect>
       </div>
     </ParentCard>
     <div class="mt-5 d-flex">
       <BaseTitle> {{ t('memberFine.title2') }} </BaseTitle>
     </div>
     <ParentCard>
-        <BaseTable
-          :headers="headers"
-          :items="finesWithStatusAndTotal"
-          :style="{ minHeight: windowHeight }"
-        >
+      <BaseTable
+        :headers="headers"
+        :items="finesWithStatusAndTotal"
+        :style="{ minHeight: windowHeight }"
+      >
         <template #[`item.time`]="{ item }">
           <span class="time-box d-inline-flex justify-center align-center">
-          <v-icon size="16" class="mr-1">mdi-clock-outline</v-icon>
-          {{ item.time }}
-        </span>
-        </template> 
+            <v-icon size="16" class="mr-1">mdi-clock-outline</v-icon>
+            {{ item.time }}
+          </span>
+        </template>
         <template #[`item.fine`]="{ item }">
           <span class="money-box d-inline-flex justify-center align-center">
-              {{ item.fine + " Ks" }}
-            </span>
-        </template> 
+            {{ item.fine + ' Ks' }}
+          </span>
+        </template>
         <template #[`item.total`]="{ item }">
           <span class="money-box d-inline-flex justify-center align-center">
-            {{ item.total  + " Ks" }}
+            {{ item.total + ' Ks' }}
           </span>
-        </template> 
-        <template #[`item.status`]="{ item }">
-            <span class="d-flex justify-left align-center"> 
-              <span
-                  class="status-label mr-5"
-                :style="{
-                        backgroundColor: item.switchValue ? 'rgba(var(--v-theme-complete), 0.2)' : 'rgba(var(--v-theme-pending), 0.2)',
-                        color:  item.switchValue ? '#789f00ff' : '#ff9800',
-                      }"
-                >
-                <v-icon size="16" class="mr-1">
-                  {{ item.switchValue ? 'mdi-check-circle-outline' : 'mdi-timer-sand' }}
-                </v-icon>
-                {{ item.switchValue ? 'Complete' : 'Pending' }}</span>
-              <span>
-                <v-switch
-                  color="primary"
-                  density="compact"
-                  hide-details="true"
-                  v-model="item.switchValue"
-                  @update:modelValue="onSwitchChange(item)"
-                  style="transform: scale(0.8);"
-                ></v-switch>
-              </span>
-            </span>
         </template>
-          <template #[`item.action`]="{ item }">
+        <template #[`item.status`]="{ item }">
+          <span class="d-flex justify-left align-center">
+            <span
+              class="status-label mr-5"
+              :style="{
+                backgroundColor: item.switchValue
+                  ? 'rgba(var(--v-theme-complete), 0.2)'
+                  : 'rgba(var(--v-theme-pending), 0.2)',
+                color: item.switchValue ? '#789f00ff' : '#ff9800',
+              }"
+            >
+              <v-icon size="16" class="mr-1">
+                {{
+                  item.switchValue
+                    ? 'mdi-check-circle-outline'
+                    : 'mdi-timer-sand'
+                }}
+              </v-icon>
+              {{ item.switchValue ? 'Complete' : 'Pending' }}</span
+            >
+            <span>
+              <v-switch
+                color="primary"
+                density="compact"
+                hide-details="true"
+                v-model="item.switchValue"
+                @update:modelValue="onSwitchChange(item)"
+                style="transform: scale(0.8)"
+              ></v-switch>
+            </span>
+          </span>
+        </template>
+        <template #[`item.action`]="{ item }">
           <span class="d-flex justify-left align-center p-0">
             <BaseButton
               elevation="0"
@@ -150,54 +157,58 @@
               :add-class="['ma-1']"
               @click.stop="showConfirmDelete(item.id)"
             >
-              <v-icon icon="tabler:IconTrash" size="18" style="color: #ff0000" />
+              <v-icon
+                icon="tabler:IconTrash"
+                size="15"
+                style="color: #ff0000"
+              />
             </BaseButton>
           </span>
-          </template>
-        </BaseTable>
+        </template>
+      </BaseTable>
     </ParentCard>
   </div>
-      <BaseConfirmDelete
-        v-model="confirmDelete"
-        :text="t('memberFine.deleteConfirmText')"
-        :class="{ 'd-none': !confirmDelete }"
-        @yes="
-          confirmDelete = false;
-          deleteMemberFine();
-        "
-        @no="
-          confirmDelete = false;
-          deleteTarget = undefined;
-        "
-      ></BaseConfirmDelete>
-      <BaseConfirmDelete
-        v-model="confirmChange"
-        :text="t('memberFine.statusConfirmText')"
-        :class="{ 'd-none': !confirmChange }"
-        @yes="
-          confirmChange = false;
-          changeStatus(switchTarget);
-        "
-        @no="
-          confirmChange = false;
-          switchTarget.switchValue = !switchTarget.switchValue;
-          switchTarget = null;
-        "
-      ></BaseConfirmDelete>
+  <BaseConfirmDelete
+    v-model="confirmDelete"
+    :text="t('memberFine.deleteConfirmText')"
+    :class="{ 'd-none': !confirmDelete }"
+    @yes="
+      confirmDelete = false;
+      deleteMemberFine();
+    "
+    @no="
+      confirmDelete = false;
+      deleteTarget = undefined;
+    "
+  ></BaseConfirmDelete>
+  <BaseConfirmDelete
+    v-model="confirmChange"
+    :text="t('memberFine.statusConfirmText')"
+    :class="{ 'd-none': !confirmChange }"
+    @yes="
+      confirmChange = false;
+      changeStatus(switchTarget);
+    "
+    @no="
+      confirmChange = false;
+      switchTarget.switchValue = !switchTarget.switchValue;
+      switchTarget = null;
+    "
+  ></BaseConfirmDelete>
 </template>
 <script setup>
-import { ref, onMounted } from 'vue'; 
+import { ref, onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useMemberStore } from '@/stores/member/member.js';
 import { useMemberFineStore } from '@/stores/member/member-fine.js';
 import { getMemberFineValidation } from '@/plugins/validations/member-fine.js';
-import {  lateTimes,months } from '@/utils/data';
+import { lateTimes, months } from '@/utils/data';
 
 const { t, locale } = useI18n();
-const memberStore = useMemberStore(); 
+const memberStore = useMemberStore();
 const items = ref([]);
 const fines = ref([]);
-const selectedName = ref(null); 
+const selectedName = ref(null);
 const selectedMonth = ref(null);
 const switchTarget = ref(null);
 const memberFineStore = useMemberFineStore();
@@ -210,32 +221,32 @@ const headers = computed(() => {
     {
       title: t('memberFine.form.name'),
       key: 'name',
-      align: 'left',  
+      align: 'left',
     },
     {
       title: t('memberFine.form.date'),
       key: 'date',
-      align: 'left', 
+      align: 'left',
     },
     {
       title: t('memberFine.form.time'),
       key: 'time',
-      align: 'left', 
+      align: 'left',
     },
     {
       title: t('memberFine.form.fine'),
       key: 'fine',
-      align: 'left', 
+      align: 'left',
     },
     {
       title: t('memberFine.form.total'),
       key: 'total',
-      align: 'left', 
+      align: 'left',
     },
     {
       title: t('memberFine.form.status'),
       key: 'status',
-      align: 'left', 
+      align: 'left',
     },
     {
       title: t('memberFine.form.action'),
@@ -259,9 +270,7 @@ if (window.innerWidth > 1366) {
   itemsCount = 5;
 }
 
-const memberFineValidation = computed(() =>
-  getMemberFineValidation(t)
-);
+const memberFineValidation = computed(() => getMemberFineValidation(t));
 
 onMounted(() => {
   fetchData();
@@ -279,31 +288,33 @@ const fetchData = async () => {
   } catch (error) {
     console.error('Error fetching members:', error);
   }
-};  
+};
 
 const fetchMemberFines = async () => {
   try {
-  const AllFine =  await memberFineStore.fetchMemberFine();
-  status.value = AllFine.data.data.length === 0 ? false : true;
-  const tmpMembersFines = memberFineStore.getMemberFine.data?.map((memberFine) => ({
-      id: memberFine.id,
-      name: memberFine.staff.eng_name,
-      date: memberFine.date,
-      time: memberFine.time,
-      status: memberFine.status,
-      total: memberFine.total,
-      fine: parseInt(memberFine.amount),
-      switchValue: memberFine.status === 1,
-    }));
+    const AllFine = await memberFineStore.fetchMemberFine();
+    status.value = AllFine.data.data.length === 0 ? false : true;
+    const tmpMembersFines = memberFineStore.getMemberFine.data?.map(
+      (memberFine) => ({
+        id: memberFine.id,
+        name: memberFine.staff.eng_name,
+        date: memberFine.date,
+        time: memberFine.time,
+        status: memberFine.status,
+        total: memberFine.total,
+        fine: parseInt(memberFine.amount),
+        switchValue: memberFine.status === 1,
+      })
+    );
     fines.value = [...tmpMembersFines];
   } catch (error) {
     console.error('Error fetching members fines:', error);
   }
-}
+};
 
 const submit = async (values) => {
   await memberFineStore.createMemberFine(values);
-  fetchMemberFines()
+  fetchMemberFines();
 };
 
 const finesWithStatusAndTotal = computed(() => {
@@ -322,8 +333,12 @@ const finesWithStatusAndTotal = computed(() => {
     const fineMonth = fineDate.getMonth() + 1;
     const fineYear = fineDate.getFullYear();
 
-    const matchName = selectedName.value ? fine.name === selectedName.value : true;
-    const matchMonth = selectedMonth.value ? fineMonth === selectedMonth.value : true;
+    const matchName = selectedName.value
+      ? fine.name === selectedName.value
+      : true;
+    const matchMonth = selectedMonth.value
+      ? fineMonth === selectedMonth.value
+      : true;
 
     const noFiltersSelected = !selectedName.value && !selectedMonth.value;
     const matchRecentMonths =
@@ -351,7 +366,7 @@ const finesWithStatusAndTotal = computed(() => {
     group.forEach((fine) => {
       runningTotal += parseFloat(fine.fine);
       fine.total = runningTotal;
-      fine.status = fine.status === 0 ? "Pending" : "Complete";
+      fine.status = fine.status === 0 ? 'Pending' : 'Complete';
     });
   });
 
@@ -377,15 +392,17 @@ const deleteMemberFine = async () => {
 const changeStatus = async (item) => {
   try {
     const newStatus = item.switchValue ? 0 : 1;
-    await memberFineStore.updateMemberFineStatus({ id: item.id, status: newStatus });
-    fetchMemberFines()
+    await memberFineStore.updateMemberFineStatus({
+      id: item.id,
+      status: newStatus,
+    });
+    fetchMemberFines();
   } catch (err) {
-    console.error("Error updating status:", err);
+    console.error('Error updating status:', err);
   } finally {
     switchTarget.value = null;
   }
 };
-
 </script>
 <style scoped>
 .no-message-switch .v-input__details {
@@ -393,28 +410,30 @@ const changeStatus = async (item) => {
 }
 .time-box {
   background-color: rgba(var(--v-theme-primary), 0.2);
-  border-radius: 6px;
-  padding: 4px 8px;
-  min-width: 90px;
-  height: 28px;
-  font-size: 11px;
-  font-weight: 500;
+  border-radius: 4px;
+  padding: 3px 9px;
+  /* min-width: 90px; */
+  /* height: 28px; */
+  font-size: 10px;
+  font-weight: 800;
   color: rgba(var(--v-theme-primary));
 }
 .money-box {
   background-color: #dbbcfb98;
-  padding: 4px 15px;
+  padding: 3px 13px;
   border-radius: 4px;
-  font-size: 11px;
-  color: #8000ffb6!important;
+  font-size: 10px;
+  color: #8000ffb6 !important;
+  font-weight: 800;
 }
 .status-label {
-  padding: 4px 5px;
-  border-radius: 6px;
-  min-width: 100px;
-  font-size: 11px;
-  font-weight: 500;
-  display: inline-block;  
+  padding: 3px;
+  border-radius: 4px;
+  min-width: 85px;
+  font-size: 10px;
+  /* font-weight: 500; */
+  display: inline-block;
   text-align: center;
+  font-weight: 800;
 }
 </style>
