ba9f077656df44a264e668e549d81655ceb5ce3c
SG-Thiha <softguide.thiha1@gmail.com>
2025-08-14 09:10:59 +0630

    Dockerization

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
-import AutoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+  },
   plugins: [
     vue(),
-    // ✅ Auto-import components from specified directories
     Components({
       dirs: ['src/components', 'src/pages', 'src/routes', 'src/store'],
       extensions: ['vue'],
       deep: true,
-      dts: 'src/components.d.ts', // Optional: for TS auto-complete
+      dts: 'src/components.d.ts',
     }),
 
-     // ✅ Auto-import Vue functions like ref, reactive, etc.
     AutoImport({
       imports: ['vue'],
-      dts: 'src/auto-imports.d.ts', // TypeScript support
+      dts: 'src/auto-imports.d.ts',
     }),
-
   ],
   resolve: {
     alias: {
-      '@': '/src'
-    }
-  }
-})
-
+      '@': '/src',
+    },
+  },
+});
